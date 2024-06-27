fb7c8d0b67e00c84fbb898fcb02f070cb0a719ca
José Mendoza V <josemv004@gmail.com>
2024-06-27 09:41:15 -0400

    commit multi promesas - no sirve

--- src/controllers/academica.controller.ts
+++ src/controllers/academica.controller.ts
@@ -1,7 +1,7 @@
 import { Request, Response } from 'express';
 import { fMateriaDocente, tMateriaDocente } from '../interfaces/pre_res.interface';
 import MateriasDocentes from '../models/tablasAcademica/materiasDocentes';
-import { literal } from 'sequelize';
+// import { literal } from 'sequelize';
 
 const actualizarMateriaDocente = async (mado: tMateriaDocente, fmad: fMateriaDocente) => {
   try {
@@ -21,10 +21,7 @@ const actualizarMateriaDocente = async (mado: tMateriaDocente, fmad: fMateriaDoc
       throw new Error('No encontrado');
     }
 
-    // console.log(`Registro Recuperado ${JSON.stringify(registro, null, 2)}`);
-
-    const fechaHora = new Date(fmad.FechaHora);
-    fechaHora.setHours(fechaHora.getHours() - 4);
+    console.log(`Registro Recuperado ${JSON.stringify(registro, null, 2)}`);
 
     const valores = {
       CodigoSEA: fmad.CodigoSea,
@@ -32,14 +29,11 @@ const actualizarMateriaDocente = async (mado: tMateriaDocente, fmad: fMateriaDoc
       TipoLlenado: fmad.TipoLlenado,
       NumeroPracticas: 1,
       NumeroLaboratorios: 1,
-      CodigoUsuarioActualizacion: fmad.Usuario,
-      FechaActualizacion: literal(`CAST('${fechaHora.toISOString()}' AS DATETIME)`)
+      CodigoUsuarioActualizacion: fmad.Usuario
     };
 
-    // console.log(valores);
-
     const actualizado = await registro.update(valores);
-    // console.log(`Registro Actualizado ${JSON.stringify(actualizado, null, 2)}`);
+    console.log(`updated record ${JSON.stringify(actualizado, null, 2)}`);
 
     return actualizado;
   } catch (error) {
@@ -48,18 +42,15 @@ const actualizarMateriaDocente = async (mado: tMateriaDocente, fmad: fMateriaDoc
   }
 };
 
-// Ejecutar actualizaciones secuencialmente
 const procesarListaMD = async (listaMD: tMateriaDocente[], materiaf: fMateriaDocente) => {
-  const results = [];
-  for (const valor of listaMD) {
-    try {
-      const result = await actualizarMateriaDocente(valor, materiaf);
-      results.push(result);
-    } catch (error) {
-      console.error(`Error al actualizar el registro: ${valor}`, error);
-    }
+  const promises = listaMD.map(valor => actualizarMateriaDocente(valor, materiaf));
+  try {
+    const results = await Promise.all(promises);
+    console.log('Todos los registros han sido actualizados');
+    return results;
+  } catch (error) {
+    console.error('Error al procesar listaMD:', error);
   }
-  return results;
 };
 
 export const guardarFormulario = (req: Request, res: Response) => {
